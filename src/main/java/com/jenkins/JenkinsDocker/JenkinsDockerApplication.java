dba1529d413653ce2df6895fcc2decc597ad94ae
Davide <davidereale12@gmail.com>
2024-09-03 12:39:56 +0200

    terzo

--- src/main/java/com/jenkins/JenkinsDocker/JenkinsDockerApplication.java
+++ src/main/java/com/jenkins/JenkinsDocker/JenkinsDockerApplication.java
@@ -1,7 +1,9 @@
 package com.jenkins.JenkinsDocker;
 
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class JenkinsDockerApplication {
@@ -9,5 +11,5 @@ public class JenkinsDockerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDockerApplication.class, args);
 	}
-
+		public String saluto = "ciao";
 }
